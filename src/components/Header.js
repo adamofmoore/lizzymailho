f91f1af6e0114ed3d71f81d7aeb4ddea5bb2194d
Adam Moore <adam@trainerroad.com>
2019-02-11 22:03:53 -0800

    quick setup

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,22 +3,19 @@ import PropTypes from 'prop-types'
 
 const Header = (props) => (
     <header id="header" style={props.timeout ? {display: 'none'} : {}}>
-        <div className="logo">
-            <span className="icon fa-diamond"></span>
-        </div>
         <div className="content">
             <div className="inner">
-                <h1>Dimension</h1>
-                <p>A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released<br />
-                for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
+                <h1>Lizzy Mailho</h1>
+                <p>Beautiful Sacramento based artist. Commissioned art and teaching painting classes.</p>
             </div>
         </div>
         <nav>
             <ul>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
+                {/* <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li> */}
+                <li><a href="mailto:lizzymailhoart@gmailcom">Email</a></li>
+                <li><a href="https://www.instagram.com/bee_whimsical/" target="_blank" rel="noopener">Instagram</a></li>
+                <li><a href="https://www.etsy.com/shop/BeeWhimsicalShop" target="_blank" rel="noopener">Etsy</a></li>
+                <li><a href="https://www.youtube.com/channel/UCVv961HlpsSCMtkEwvRio2A" target="_blank" rel="noopener">YouTube</a></li>
             </ul>
         </nav>
     </header>
