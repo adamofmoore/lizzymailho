d14577c3ddd6c5baf5fed248c132523e7ba54405
Adam Moore <adam@trainerroad.com>
2019-03-23 13:31:24 -0700

    other changes for header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,16 +5,18 @@ const Header = (props) => (
     <header id="header" style={props.timeout ? {display: 'none'} : {}}>
         <div className="content">
             <div className="inner">
-                <h1>Lizzy Mailho</h1>
-                <p>Sacramento based paint whisperer. Contact me to commission work or book a paint party.</p>
+                <h1>Painting with Lizzy</h1>
+                <p>Sacramento based artist and instructor for step by step painting parties. Contact me for booking or questions.</p>
             </div>
         </div>
         <nav>
             <ul>
-                <li><a href="mailto:lizzymailhoart@gmailcom">Email</a></li>
-                <li><a href="https://www.instagram.com/bee_whimsical/" target="_blank" rel="noopener">Instagram</a></li>
+                {/* <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li>
+                <li><a href="/page-2">Page 2</a></li> */}
+                <li><a href="mailto:paintingwithlizzy@gmailcom">Email</a></li>
+                <li><a href="https://www.instagram.com/painting_with_lizzy/" target="_blank" rel="noopener">Instagram</a></li>
                 <li><a href="https://www.etsy.com/shop/BeeWhimsicalShop" target="_blank" rel="noopener">Etsy</a></li>
-                <li><a href="https://www.youtube.com/channel/UCVv961HlpsSCMtkEwvRio2A" target="_blank" rel="noopener">YouTube</a></li>
+                {/* <li><a href="https://www.youtube.com/channel/UCVv961HlpsSCMtkEwvRio2A" target="_blank" rel="noopener">YouTube</a></li> */}
             </ul>
         </nav>
     </header>
